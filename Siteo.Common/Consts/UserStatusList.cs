07269e7122e3d849da075152412e88cf769c1adb
leolin0704 <84984903@qq.com>
2018-12-11 22:48:21 +0800

    add const to common

---
+++ Siteo.Common/Consts/UserStatusList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siteo.Common.Consts
+{
+    public static class UserStatusList
+    {
+        public const string Active = "A";
+        public const string Inactive = "I";
+    }
+}
